f4998919b5b3356199004828b264221092bb30df
Kimhanjin <ddang0103@naver.com>
2024-03-22 01:39:30 +0900

    Login Test Code 작성 중간 저장

--- src/test/java/com/movie/movieinfo/LoginTestCode.java
+++ src/test/java/com/movie/movieinfo/LoginTestCode.java
@@ -1,46 +1,66 @@
 package com.movie.movieinfo;
 
-import com.movie.movieinfo.service.movie.MovieListService;
-import org.junit.jupiter.api.BeforeEach;
+import com.movie.movieinfo.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@Import(SecurityConfig.class)
 public class LoginTestCode {
+
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext context;
-
-
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity()) // 스프링 시큐리티 적용
-                .build();
+    @Test
+    public void shouldPermitAllForLoginPage() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk());
     }
 
     @Test
-    @WithMockUser
-    public void loginSuccessTest() throws Exception {
+    public void shouldPermitAllForHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser // 가짜 인증된 사용자를 사용하여 인증된 요청을 시뮬레이션합니다.
+    public void shouldAllowAuthenticatedRequestToAnyPage() throws Exception {
+        mockMvc.perform(get("/anyPage"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldRedirectToLoginForUnauthenticatedRequests() throws Exception {
+        mockMvc.perform(get("/anyPage"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    public void shouldLoginSuccess() throws Exception {
         mockMvc.perform(formLogin("/login").user("user").password("password"))
-                .andExpect(status().isFound()) // 로그인 성공시 리다이렉트 되므로 302 상태 코드 확인
-                .andExpect(redirectedUrl("/")); // 기본 성공 URL로 리다이렉트
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void shouldLogoutSuccessfully() throws Exception {
+        mockMvc.perform(post("/logout")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
     }
 }
