7aeb3e5f828f36b63cd03c003a9af5101068e39d
Kimhanjin <ddang0103@naver.com>
2024-03-24 19:18:35 +0900

    카카오 소셜 로그인 기능 추가

--- src/main/java/com/movie/movieinfo/service/user/OAuth2Service.java
+++ src/main/java/com/movie/movieinfo/service/user/OAuth2Service.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -15,13 +16,20 @@ public class OAuth2Service extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
         System.out.println("userRequest:" + userRequest);
         System.out.println("userRequest clientRegistration:" + userRequest.getClientRegistration());
         System.out.println("userRequestAccessToken::::::"+userRequest.getAccessToken());
         System.out.println("userRequestAttributes():::::::::::::"+super.loadUser(userRequest).getAttributes());
         System.out.println("oAuth2User.getAttributes():::::::::::::::::::::::::::::::::::" +oAuth2User.getAttributes());
-        var authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
-        return new DefaultOAuth2User(authorities, oAuth2User.getAttributes(), "sub");
+        if("google".equals(registrationId)) {
+            var authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+            return new DefaultOAuth2User(authorities, oAuth2User.getAttributes(), "sub");
+        } else if ("kakao".equals(registrationId)) {
+            var authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+            return new DefaultOAuth2User(authorities, oAuth2User.getAttributes(), "id");
+        }
+        throw new OAuth2AuthenticationException(new OAuth2Error("Unsupported_Login_Provider" ,"지원하지 않는 로그인 방식입니다", null));
     }
 
 }
