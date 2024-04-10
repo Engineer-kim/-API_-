d490c0d20502da56947d092732f88f7f78b65117
Kimhanjin <ddang0103@naver.com>
2024-04-11 00:48:14 +0900

    fix: Corsconfig 메서드 .allowedOriginPatterns("*") 로 변경

--- src/main/java/com/movie/movieinfo/config/CorsConfig.java
+++ src/main/java/com/movie/movieinfo/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/movieInfo/**")
-                .allowedOrigins("*") // 모든 도메인에서의 접근을 허용
+                .allowedOriginPatterns("*") // 모든 도메인에서의 접근을 허용
                 .allowedMethods("GET")
                 .allowCredentials(true)
                 .maxAge(3600); // pre-flight 요청의 결과를 캐시하는 시간(초)
