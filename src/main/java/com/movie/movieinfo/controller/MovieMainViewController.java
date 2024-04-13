ebdea940045496f8b1d16cb4d0051f33c27ffad5
Kimhanjin <ddang0103@naver.com>
2024-04-13 22:04:09 +0900

    영화 일별 순위 표출 및 각 영화 별 상세 페이지로 이동하는 로직추가
    # 상세 페이지 이동시 식별자인 MOvieCd 값을 넘겨 상세페이지로 이동함
    # 디자인 요소 빠져있음

--- src/main/java/com/movie/movieinfo/controller/MovieMainViewController.java
+++ src/main/java/com/movie/movieinfo/controller/MovieMainViewController.java
@@ -3,15 +3,22 @@ package com.movie.movieinfo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/movieInfoMain")
 public class MovieMainViewController {
 
-    /**메인*/
+    /**영화 메인*/
     @GetMapping("/main")
     public String movieMain() {
         return "movieInfoMain";
+    }
+
+    /**영화 상세*/
+    @GetMapping("/detail")
+    public String movieDetail(@RequestParam("movieCd")  String movieCd) {
+        return "movieInfoDetail";
     }
 
 }
