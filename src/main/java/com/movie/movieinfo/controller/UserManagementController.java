b547028862cb57f22a8fb5ced53ac537b0e92013
Kimhanjin <ddang0103@naver.com>
2024-05-11 15:42:57 +0900

    비밀번호 초기화를 위한 이메일 입력창
    비밀번호 초기화를 위한 이메일 입력하는 화면단 API

--- src/main/java/com/movie/movieinfo/controller/UserManagementController.java
+++ src/main/java/com/movie/movieinfo/controller/UserManagementController.java
@@ -14,10 +14,16 @@ public class UserManagementController {
         return "findUserId.html";
     }
     
-    
-    /**비밀 번호 재설정 화면*/
+
+    /**비밀번호 찿기를 위한 이메일 입력 화면*/
     @GetMapping("/resetPasswordView")
     public String resetPasswordView() {
         return "resetPasswordView.html";
+    }
+
+    /**비밀 번호 재설정 화면*/
+    @GetMapping("/resetNewPasswordView")
+    public String resetNewPasswordView() {
+        return "resetNewPasswordView.html";
     }
 }
